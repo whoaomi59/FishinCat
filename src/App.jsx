ccd95c92fd848db7c5349ec0badd6e40e9713f81
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-10-05 11:48:19 -0500

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="conter">
       <h1 className="animate__animated animate__bounce">Hola Hija</h1>
-      <a href="/upload/florl.rar">Descargar Fichero</a>
+      <a href="/upload/win-amd64-3.12.rar">Descargar Ficheros</a>
     </div>
   );
 }
