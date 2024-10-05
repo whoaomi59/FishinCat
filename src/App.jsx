1fdbad2079af85c9791def7d3f4307800884ec7b
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-10-05 10:51:41 -0500

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,7 @@ function App() {
   return (
     <div className="conter">
       <h1 className="animate__animated animate__bounce">Hola Hija</h1>
-      <a href="/upload/flower.rar">Descargar Fichero</a>
-      <br />
-      <br />
-      <br />
-      <a href="/upload/flower.rar">Descargar Fichero2</a>
-      <br />
-      <br />
-      <br />
-      <a href="/upload/TroyanFLower.rar">Descargar Fichero33</a>
+      <a href="/upload/FLower.zip">Descargar Fichero</a>
     </div>
   );
 }
